9ff9c93ce54769902f8f21597bfecb03bd56ea4b
nadiellymedeiros <nadiellymedeiros@gmail.com>
2023-07-06 15:33:53 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+import style from "./App.module.css";
+function App() {
+  return (
+    <div className={style.app}>
+      <h1>Github Finder</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,37 @@
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+import { useState, KeyboardEvent } from "react";
+
+import { BsSearch } from "react-icons/bs";
+
+import style from "./Search.module.css";
+
+export default function Search({ loadUser }: SearchProps) {
+  const [userName, setUserName] = useState("");
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loadUser(userName);
+    }
+  };
+
+  return (
+    <div className={style.search}>
+      <h2>Busque por um usuário:</h2>
+      <p>conheça seus melhores repositórios</p>
+      <div className={style.search_container}>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  );
+}
